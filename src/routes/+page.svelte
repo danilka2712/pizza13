a30cdb4525cf320944df55433fce3ab68f0d6301
unknown <danilka2712@yandex.ru>
2022-12-02 14:20:06 +0600

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -26,15 +26,15 @@
 			<div
 				class=" relative z-0 bg-[#232323] border-[#333333] border flex  sm:flex-col justify-between  rounded-xl "
 			>
-				<div class="w-44 h-full rounded-xl">
-					<img class="object-cover rounded-xl h-full w-full p-2" src="sushi.webp" alt="" />
+				<div class="w-44 h-full rounded-lg">
+					<img class="object-cover rounded-lg h-full w-full p-2" src="sushi.webp" alt="" />
 				</div>
 				<div class="p-2 ">
 					<a class="" href="/">Суши набор</a>
 					<p class="text-xs text-gray-400 pr-14">Описание товара, суши роллы</p>
 					<div class="flex items-center justify-between">
 						<p class=" text-white text-lg">1990 ₽</p>
-						<button class=" text-sm bg-red-700 px-4 py-2 rounded-xl">Добавить</button>
+						<button class=" text-sm bg-red-700 px-4 py-2 rounded-lg">Добавить</button>
 					</div>
 				</div>
 			</div>
