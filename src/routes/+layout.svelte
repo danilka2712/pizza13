3b267340cebba6ea8fb8503196be48126b79ee4f
unknown <danilka2712@yandex.ru>
2022-12-02 13:11:22 +0600

    first commit

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -22,28 +22,7 @@
 					</div>
 				</div>
 				<!-- Responsive navbar -->
-				<a class="xl:hidden flex mr-6 items-center" href="#">
-					<svg
-						xmlns="http://www.w3.org/2000/svg"
-						class="h-6 w-6 hover:text-gray-200"
-						fill="none"
-						viewBox="0 0 24 24"
-						stroke="currentColor"
-					>
-						<path
-							stroke-linecap="round"
-							stroke-linejoin="round"
-							stroke-width="2"
-							d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z"
-						/>
-					</svg>
-					<span class="flex absolute -mt-5 ml-4">
-						<span
-							class="animate-ping absolute inline-flex h-3 w-3 rounded-full bg-pink-400 opacity-75"
-						/>
-						<span class="relative inline-flex rounded-full h-3 w-3 bg-pink-500" />
-					</span>
-				</a>
+				
 				<a class="navbar-burger self-center xl:hidden" href="#">
 					<svg
 						xmlns="http://www.w3.org/2000/svg"
@@ -65,10 +44,18 @@
 	</div>
 </div>
 <nav class="box  fixed  w-full bg-[#1b1b1b]  px-10 py-5 pb-7 bottom-0">
-	<a href="/"><img width="22" src="home-2.svg" alt="" /></a>
-	<a href="/"><img width="22" src="shopping-bag.svg" alt="" /></a>
-	<a href="/"><img width="22" src="gps.svg" alt="" /></a>
-	<a href="/"><img width="22" src="frame.svg" alt="" /></a>
+	<a class=" text-xs text-[#f9f9f9] gap-1 flex flex-col justify-center items-center" href="/"
+		><img width="22" src="home-2.svg" alt="" />Главная</a
+	>
+	<a class=" text-xs text-[#f9f9f9] gap-1 flex flex-col justify-center items-center" href="/"
+		><img width="22" src="shopping-bag.svg" alt="" />Корзина</a
+	>
+	<a class=" text-xs text-[#f9f9f9] gap-1 flex flex-col justify-center items-center" href="/"
+		><img width="22" src="gps.svg" alt="" />Заказы</a
+	>
+	<a class=" text-xs text-[#f9f9f9] gap-1 flex flex-col justify-center items-center" href="/"
+		><img width="16" src="frame.svg" alt="" />Кабинет</a
+	>
 </nav>
 
 <slot />
