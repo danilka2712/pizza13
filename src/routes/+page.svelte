0feab330c109cf8fc4b70bcff094e37a7c48dd69
unknown <danilka2712@yandex.ru>
2022-12-07 22:33:05 +0600

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,41 +3,49 @@
 		{
 			name: 'Эвакуатор',
 			description: 'Доставим ваш автомобиль в любой точку России',
-			img: 'car3.svg'
+			img: 'car3.svg',
+			href: '/dashboard'
 		},
 		{
 			name: 'Грузоперевозка',
 			description: 'Доставим ваш автомобиль в любой точку России',
-			img: 'car1.svg'
+			img: 'car1.svg',
+			href: '/'
 		},
 		{
 			name: 'Отогрев авто',
 			description: 'Доставим ваш автомобиль в любой точку России',
-			img: 'car2.svg'
+			img: 'car2.svg',
+			href: '/'
 		},
 		{
 			name: 'Автоподбор',
 			description: 'Доставим ваш автомобиль в любой точку России',
-			img: 'car4.svg'
+			img: 'car4.svg',
+			href: '/'
 		}
 	];
 </script>
 
 <div class="px-5">
 	<div class=" bg-[#5BC43A] h-44 border-[#e8e8e8]/75 border rounded-xl">
-		<img class="object-cover rounded-xl w-full h-full" src="padarki.svg" alt="">
+		<img class="object-cover rounded-xl w-full h-full" src="padarki.svg" alt="" />
 	</div>
 	<div class=" flex flex-col gap-5 mb-22 mt-6">
 		{#each items as item}
-			<div class="p-6 relative bg-[#F7F7F8] border-[#e8e8e8]/75 border flex rounded-2xl ">
-				<div>
-					<p class=" text-lg font-semibold mb-2">{item.name}</p>
-					<p class="text-sm text-[#8e8e8e]">Доставим ваш автомобиль <br /> в любой точку России</p>
-				</div>
-				<div class="">
-					<img class="w-32 absolute right-1 top-4" src={item.img} alt="" />
-				</div>
-			</div>
+			<a href={item.href}>
+				<div class="p-6 relative bg-[#F7F7F8] border-[#e8e8e8]/75 border flex rounded-2xl ">
+					<div>
+						<p class=" text-lg font-semibold mb-2">{item.name}</p>
+						<p class="text-sm text-[#8e8e8e]">
+							Доставим ваш автомобиль <br /> в любой точку России
+						</p>
+					</div>
+					<div class="">
+						<img class="w-32 absolute right-1 top-4" src={item.img} alt="" />
+					</div>
+				</div></a
+			>
 		{/each}
 	</div>
 </div>
--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,1 +1,21 @@
-<div>Hello</div>
+<script>
+
+</script>
+
+<div class="px-5">
+    <div>
+        <h1 class=" text-xl my-4 mb-7">Оставить заявку</h1>
+    </div>
+    <div class="flex mb-8 gap-2 flex-col">
+        <span>Имя</span>
+        <input class="p-2 py-4 rounded-2xl" type="text" name="" id="">
+    </div>
+    <div class="flex mb-8 gap-2 flex-col">
+        <span>Марка авто</span>
+        <input class="p-2 py-4 rounded-2xl" type="text" name="" id="">
+    </div>
+    <div class="px-5 ">
+    <button class=" bg-[#5BC43A] p-3 rounded-2xl w-[90%] py-4 fixed bottom-28 mx-5 left-0 right-0 font-semibold text-white">Оставить заявку</button>
+
+    </div>
+</div>
