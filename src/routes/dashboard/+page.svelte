869e75b3417601943c528b47e3667fc0ad6dee1f
unknown <danilka2712@yandex.ru>
2022-12-08 10:05:25 +0600

    first commit

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,34 +1,64 @@
 <script>
-
 </script>
 
 <div class="px-5">
-    <div>
-        <h1 class=" font-bold text-xl my-4 mb-7">Заказать Эвакуатор</h1>
-    </div>
-    
-    <div class="flex mb-6  flex-col">
-        <span class="text-[#8e8e8e] mb-3 text-sm">Контактные данные</span>
-        <input placeholder="Ваше имя" class="p-4 mb-3 border-[#e8e8e8]/75 border py-4 rounded-2xl" type="text" name="" id="">
-        <input placeholder="Номер телефона" class="p-4 border-[#e8e8e8]/75 border py-4 rounded-2xl" type="text" name="" id="">
-    </div>
-    <div class="flex mb-6">
-        <div>
-            <span class="text-[#8e8e8e] mb-3 text-sm">Откуда</span>
-            <input placeholder="Омск, ул.Мира" class="p-4  border-[#e8e8e8]/75 border py-4 rounded-2xl" type="text" name="" id="">
-        </div>
-        <div>
-            <span class="text-[#8e8e8e]  text-sm">Куда</span>
-            <input placeholder="Омск, ул.Лукашевича" class="p-4  border-[#e8e8e8]/75 border py-4 rounded-2xl" type="text" name="" id="">
-        </div>
+	<div>
+		<h1 class=" font-bold text-xl my-4 mb-7">Заказать Эвакуатор</h1>
+	</div>
 
-    </div>
-    <div class="flex mb-8 flex-col">
-        <span  class="text-[#8e8e8e] mb-3  text-sm">Марка авто</span>
-        <input placeholder="Nissan Sunny"  class="p-4 border-[#e8e8e8]/75 border py-4 rounded-2xl" type="text" name="" id="">
-    </div>
-    <div class="px-5 ">
-    <button class=" bg-[#5BC43A] p-3 rounded-2xl w-[90%] py-4 fixed bottom-28 mx-5 left-0 right-0 font-semibold text-white">Оставить заявку</button>
-
-    </div>
+	<div class="flex mb-6  flex-col">
+		<span class="text-[#8e8e8e] mb-3 text-sm">Контактные данные</span>
+		<input
+			placeholder="Ваше имя"
+			class="p-4 mb-3 border-[#e8e8e8]/75 border py-4 rounded-2xl"
+			type="text"
+			name=""
+			id=""
+		/>
+		<input
+			placeholder="Номер телефона"
+			class="p-4 border-[#e8e8e8]/75 border py-4 rounded-2xl"
+			type="text"
+			name=""
+			id=""
+		/>
+	</div>
+	<div class="grid grid-cols-2 gap-4 mb-6">
+		<div class="flex flex-col">
+			<span class="text-[#8e8e8e] mb-3 text-sm">Откуда</span>
+			<input
+				placeholder="Омск, ул.Мира"
+				class=" border-[#e8e8e8]/75 w-[100%] border p-4 rounded-2xl"
+				type="text"
+				name=""
+				id=""
+			/>
+		</div>
+		<div class="flex flex-col">
+			<span class="text-[#8e8e8e] mb-3  text-sm">Куда</span>
+			<input
+				placeholder="Омск, ул.Лукашевича"
+				class=" border-[#e8e8e8]/75 w-[100%] p-4  border rounded-2xl"
+				type="text"
+				name=""
+				id=""
+			/>
+		</div>
+	</div>
+	<div class="flex mb-8 flex-col">
+		<span class="text-[#8e8e8e] mb-3  text-sm">Марка авто</span>
+		<input
+			placeholder="Nissan Sunny"
+			class="p-4 border-[#e8e8e8]/75 border py-4 rounded-2xl"
+			type="text"
+			name=""
+			id=""
+		/>
+	</div>
+	<div class="px-5 ">
+		<button
+			class=" bg-[#5BC43A] p-3 rounded-2xl w-[90%] py-4 fixed bottom-28 mx-5 left-0 right-0 font-semibold text-white"
+			>Оставить заявку</button
+		>
+	</div>
 </div>
