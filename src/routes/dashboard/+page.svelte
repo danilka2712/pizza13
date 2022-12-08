a68b8d0343366a81d444a75cf687d18911f75c39
unknown <danilka2712@yandex.ru>
2022-12-08 11:52:51 +0600

    first commit

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -58,20 +58,19 @@
 		</div>
 	</div>
     <div class="flex justify-center">
-        <div class="mb-3 xl:w-96">
+        <div class="mb-3 w-full">
           <select class="form-select appearance-none
             block
             w-full
-            px-3
-            py-1.5
+            px-4
+            py-4
             text-base
             font-normal
             text-gray-700
             bg-white bg-clip-padding bg-no-repeat
             border border-solid border-gray-300
-            rounded
             transition
-            ease-in-out
+            ease-in-out rounded-xl
             m-0
             focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none" aria-label="Default select example">
               <option selected>Open this select menu</option>
