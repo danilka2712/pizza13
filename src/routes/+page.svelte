fc71532a25ede19120094619e462a994c0c4aa50
unknown <danilka2712@yandex.ru>
2022-12-08 10:06:37 +0600

    first commit

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,7 +31,7 @@
 	<div class=" bg-[#5BC43A] h-44 border-[#e8e8e8]/75 border rounded-xl">
 		<img class="object-cover rounded-xl w-full h-full" src="padarki.svg" alt="" />
 	</div>
-	<div class=" flex flex-col gap-5 mb-22 mt-6">
+	<div class=" flex flex-col gap-5 mb-24 mt-6">
 		{#each items as item}
 			<a href={item.href}>
 				<div class="p-6 relative bg-[#F7F7F8] border-[#e8e8e8]/75 border flex rounded-2xl ">
