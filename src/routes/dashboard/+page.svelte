e8671de932a0a11df6697172f7546451431cd451
unknown <danilka2712@yandex.ru>
2022-12-08 12:10:40 +0600

    Initial commit

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -57,9 +57,11 @@
 			/>
 		</div>
 	</div>
-	<div class="flex justify-center">
+	<div class="flex flex-col justify-center">
+		<span class="text-[#8e8e8e] mb-3  text-sm">Марка автомобиля</span>
+
 		<div class="mb-3 w-full">
-			<select size="6"
+			<select
 				class="form-select appearance-none
             block
             w-full
@@ -67,7 +69,6 @@
             py-4
             text-base
             font-normal
-            text-gray-700
             bg-white bg-clip-padding bg-no-repeat
             border border-solid border-[#e8e8e8]
             transition
@@ -76,7 +77,7 @@
             focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
 				aria-label="Default select example"
 			>
-				<option selected>Выберите модель автомобиля</option>
+				<option class="" selected>Acura</option>
 				{#each item as i}
 					<option name="" id="">{i}</option>
 				{/each}
@@ -85,7 +86,7 @@
 	</div>
 	<div class="px-5 ">
 		<button
-			class=" bg-[#5BC43A] p-3 rounded-2xl w-[90%] py-4 fixed bottom-28 mx-5 left-0 right-0 font-semibold text-white"
+			class=" bg-[#5BC43A] p-3 rounded-2xl w-[90%] sm:w-[97%] py-4 fixed bottom-28 mx-5 left-0 right-0 font-semibold text-white"
 			>Оставить заявку</button
 		>
 	</div>
