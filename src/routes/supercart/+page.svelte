767bf3bdfe61c89d730425eaa4fbcc07f097fc56
unknown <danilka2712@yandex.ru>
2022-12-13 21:28:11 +0600

    Initial commit

--- src/routes/supercart/+page.svelte
+++ src/routes/supercart/+page.svelte
@@ -2,11 +2,21 @@
 	import { onMount } from 'svelte';
 	import maplibregl from 'maplibre-gl';
 	import 'maplibre-gl/dist/maplibre-gl.css';
-	import DistanceMeasurementMapLibreGlDirections, {
-		config
-	} from '../../lib/map/distance-measurement-directions';
-	let directions: DistanceMeasurementMapLibreGlDirections | undefined = undefined;
-	let totalDistance = 0;
+	import MapLibreGlDirections from '@maplibre/maplibre-gl-directions';
+	let mapRef: HTMLElement | undefined = undefined;
+	let map: maplibregl.Map | undefined = undefined;
+	let directions: MapLibreGlDirections | undefined = undefined;
+	let interactive = true;
+	let refreshOnMove = false;
+	function changeRefreshOnMove() {
+		if (directions) directions.destroy();
+		directions = new MapLibreGlDirections(map, {
+			requestOptions: {
+				alternatives: 'true'
+			},
+			refreshOnMove: !refreshOnMove
+		});
+	}
 	onMount(() => {
 		const map = new maplibregl.Map({
 			container: 'map', // container id
@@ -14,19 +24,23 @@
 			center: [73.37081369020865, 54.99035944909227], // starting position [lng, lat]
 			zoom: 12 // starting zoom
 		});
+
 		map.on('load', () => {
-			directions = new DistanceMeasurementMapLibreGlDirections(map, config);
-			directions.on('fetchroutesend', (ev) => {
-				totalDistance = ev.data?.routes[0].distance as number;
-			});
-			directions.on('removewaypoint', () => {
-				if (directions.waypoints.length < 2) {
-					totalDistance = 0;
+			directions = new MapLibreGlDirections(map, {
+				requestOptions: {
+					alternatives: 'true'
 				}
 			});
-			directions.interactive = true;
 		});
 	});
+	$: if (directions) {
+		directions.interactive = interactive;
+	}
 </script>
+
+
+
+
+<div bind:this={mapRef} class="basis-full lg:basis-2/3 shadow-xl" />
 
 <div id="map" class="w-full h-screen" />
