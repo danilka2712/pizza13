49dd389cb760f7aef780b7485bca1b12f233abc2
unknown <danilka2712@yandex.ru>
2022-12-14 10:25:55 +0600

    Initial commit

--- src/lib/components/InputAddress.svelte
+++ src/lib/components/InputAddress.svelte
@@ -17,8 +17,6 @@
 		src="https://api.mapbox.com/search-js/v1.0.0-beta.14/web.js"
 	></script>
 </svelte:head>
-<div id="map" style="width: 400px; height: 300px;" />
-
 <div class="grid grid-cols-1 gap-3 mb-6">
 	<div class="flex flex-col">
 		<span class="text-[#8e8e8e] mb-3 text-sm">Укажите маршрут</span>
@@ -29,7 +27,7 @@
 					access-token="pk.eyJ1IjoiZGFuaWxrYTI3MTIiLCJhIjoiY2xiamFndWc2MDJoazNwcXZnaXZoNm9hYSJ9.lAMLaj7C67amMgE1yWU_WA"
 				>
 					<input
-						autocomplete="street-address1"
+						autocomplete="city state street-address1"
 						placeholder="Омск, ул.Мира"
 						class=" border-[#e8e8e8]/75 font-sans focus:border-[#5BC43A] pl-24 focus:outline-none w-[100%] border p-4 rounded-2xl"
 						type="text"
--- src/routes/supercart/+page.svelte
+++ src/routes/supercart/+page.svelte
@@ -1,46 +1,75 @@
 <script lang="ts">
+	import mapboxgl from 'mapbox-gl';
 	import { onMount } from 'svelte';
-	import maplibregl from 'maplibre-gl';
-	import 'maplibre-gl/dist/maplibre-gl.css';
-	import MapLibreGlDirections from '@maplibre/maplibre-gl-directions';
-	let mapRef: HTMLElement | undefined = undefined;
-	let map: maplibregl.Map | undefined = undefined;
-	let directions: MapLibreGlDirections | undefined = undefined;
-	let interactive = true;
-	let refreshOnMove = false;
-	function changeRefreshOnMove() {
-		if (directions) directions.destroy();
-		directions = new MapLibreGlDirections(map, {
-			requestOptions: {
-				alternatives: 'true'
-			},
-			refreshOnMove: !refreshOnMove
+	mapboxgl.accessToken =
+		'pk.eyJ1IjoiZGFuaWxrYTI3MTIiLCJhIjoiY2xiamFndWc2MDJoazNwcXZnaXZoNm9hYSJ9.lAMLaj7C67amMgE1yWU_WA';
+	function updateSet() {
+		const map = new mapboxgl.Map({
+			container: 'map',
+			// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+			style: 'https://api.maptiler.com/maps/voyager-v2/style.json?key=EfH47Bb8jzv9Pl57bst7',
+			center: [73.28537735766437, 55.00392640477281],
+			minZoom: 8,
+			zoom: 12
+		});
+		let marker = new mapboxgl.Marker().setLngLat([0, 0]).addTo(map);
+		map.on('move', function (e) {
+			let coord = marker.setLngLat(map.getCenter());
+			coordinat = coord.getLngLat();
 		});
 	}
-	onMount(() => {
-		const map = new maplibregl.Map({
-			container: 'map', // container id
-			style: 'https://api.maptiler.com/maps/voyager-v2/style.json?key=EfH47Bb8jzv9Pl57bst7', // style URL
-			center: [73.37081369020865, 54.99035944909227], // starting position [lng, lat]
-			zoom: 12 // starting zoom
-		});
+	let coordinat;
+	let coordinat1;
 
-		map.on('load', () => {
-			directions = new MapLibreGlDirections(map, {
-				requestOptions: {
-					alternatives: 'true'
-				}
-			});
+	function addCoordinat() {
+		const map = new mapboxgl.Map({
+			container: 'map',
+			// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+			style: 'https://api.maptiler.com/maps/voyager-v2/style.json?key=EfH47Bb8jzv9Pl57bst7',
+			center: coordinat,
+			minZoom: 8,
+			zoom: 12
 		});
-	});
-	$: if (directions) {
-		directions.interactive = interactive;
+		new mapboxgl.Marker().setLngLat(coordinat).addTo(map);
+		let marker = new mapboxgl.Marker().setLngLat([0, 0]).addTo(map);
+		map.on('move', function (e) {
+			let coord = marker.setLngLat(map.getCenter());
+			coordinat1 = coord.getLngLat();
+		});
+	}
+	function addCoordinat2() {
+		const map = new mapboxgl.Map({
+			container: 'map',
+			// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+			style: 'https://api.maptiler.com/maps/voyager-v2/style.json?key=EfH47Bb8jzv9Pl57bst7',
+			center: coordinat,
+			minZoom: 8,
+			zoom: 12
+		});
+		new mapboxgl.Marker({
+			draggable: true
+		})
+			.setLngLat(coordinat)
+			.addTo(map);
+
+		const marker = new mapboxgl.Marker({
+			draggable: true
+		})
+			.setLngLat(coordinat1)
+			.addTo(map);
+
+		const coordinates = document.getElementById('coordinates');
+		function onDragEnd() {
+			const lngLat = marker.getLngLat();
+		}
+
+		marker.on('dragend', onDragEnd);
 	}
 </script>
 
+<pre id="coordinates" class="coordinates" />
+<button on:click={() => addCoordinat2()}>Вторая точка</button>
+<button on:click={() => addCoordinat()}>Первая точка</button>
+<button on:click={() => updateSet()}>Карта</button>
 
-
-
-<div bind:this={mapRef} class="basis-full lg:basis-2/3 shadow-xl" />
-
-<div id="map" class="w-full h-screen" />
+<div id="map" class=" w-full h-screen" />
