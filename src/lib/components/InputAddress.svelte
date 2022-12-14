9669579515849ca3d6fe2e09ed0657e3205fe72a
unknown <danilka2712@yandex.ru>
2022-12-14 19:02:23 +0600

    first commit

--- src/lib/components/InputAddress.svelte
+++ src/lib/components/InputAddress.svelte
@@ -1,3 +1,170 @@
-<div>
-	<h1>HEllo</h1>
+<script lang="ts">
+	import mapboxgl from 'mapbox-gl';
+	mapboxgl.accessToken =
+		'pk.eyJ1IjoiZGFuaWxrYTI3MTIiLCJhIjoiY2xiamFndWc2MDJoazNwcXZnaXZoNm9hYSJ9.lAMLaj7C67amMgE1yWU_WA';
+	function updateSet() {
+		ofButton += 1;
+		hidden = !hidden;
+		if (hidden === true) {
+			setTimeout(() => {
+				const map = new mapboxgl.Map({
+					container: 'map',
+					// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+					style: 'https://api.maptiler.com/maps/voyager-v2/style.json?key=EfH47Bb8jzv9Pl57bst7',
+					center: [73.28537735766437, 55.00392640477281],
+					minZoom: 8,
+					zoom: 12
+				});
+				map.addControl(
+					new mapboxgl.GeolocateControl({
+						positionOptions: {
+							enableHighAccuracy: true
+						},
+						// When active the map will receive updates to the device's location as it changes.
+						trackUserLocation: true,
+						// Draw an arrow next to the location dot to indicate which direction the device is heading.
+						showUserHeading: true
+					})
+				);
+
+				let marker = new mapboxgl.Marker().setLngLat([0, 0]).addTo(map);
+				map.on('move', function (e) {
+					let coord = marker.setLngLat(map.getCenter());
+					coordinat = coord.getLngLat();
+					console.log();
+				});
+			}, 500);
+		}
+	}
+	let coordinat;
+	let coordinat1;
+
+	function addCoordinat() {
+		ofButton += 1;
+		const map = new mapboxgl.Map({
+			container: 'map',
+			// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+			style: 'https://api.maptiler.com/maps/voyager-v2/style.json?key=EfH47Bb8jzv9Pl57bst7',
+			center: coordinat,
+			minZoom: 8,
+			zoom: 12
+		});
+		fetch(
+			`https://api.mapbox.com/geocoding/v5/mapbox.places/${coordinat.toArray()}.json?types=address%2Cpoi%2Cdistrict%2Cneighborhood&limit=1&access_token=pk.eyJ1IjoiZGFuaWxrYTI3MTIiLCJhIjoiY2xiamFndWc2MDJoazNwcXZnaXZoNm9hYSJ9.lAMLaj7C67amMgE1yWU_WA`
+		)
+			.then((data) => data.json())
+			.then((coordin) => (address1 = coordin.features[0].place_name));
+
+		new mapboxgl.Marker().setLngLat(coordinat).addTo(map);
+		let marker = new mapboxgl.Marker().setLngLat([0, 0]).addTo(map);
+		map.on('move', function (e) {
+			let coord = marker.setLngLat(map.getCenter());
+			coordinat1 = coord.getLngLat();
+		});
+	}
+	function addCoordinat2() {
+		ofButton = 1;
+		const map = new mapboxgl.Map({
+			container: 'map',
+			// Choose from Mapbox's core styles, or make your own style with Mapbox Studio
+			style: 'https://api.maptiler.com/maps/voyager-v2/style.json?key=EfH47Bb8jzv9Pl57bst7',
+			center: coordinat,
+			minZoom: 8,
+			zoom: 12
+		});
+
+		fetch(
+			`https://api.mapbox.com/directions/v5/mapbox/driving/${coordinat.toArray()};${coordinat1.toArray()}?access_token=pk.eyJ1IjoiZGFuaWxrYTI3MTIiLCJhIjoiY2xiamFndWc2MDJoazNwcXZnaXZoNm9hYSJ9.lAMLaj7C67amMgE1yWU_WA`
+		)
+			.then((data) => data.json())
+			.then((coss) => (distance = coss.routes[0].distance));
+		fetch(
+			`https://api.mapbox.com/geocoding/v5/mapbox.places/${coordinat1.toArray()}.json?types=address%2Cpoi%2Cdistrict%2Cneighborhood&limit=1&access_token=pk.eyJ1IjoiZGFuaWxrYTI3MTIiLCJhIjoiY2xiamFndWc2MDJoazNwcXZnaXZoNm9hYSJ9.lAMLaj7C67amMgE1yWU_WA`
+		)
+			.then((data) => data.json())
+			.then((coordin) => (address2 = coordin.features[0].place_name));
+
+		new mapboxgl.Marker({
+			draggable: true
+		})
+			.setLngLat(coordinat)
+			.addTo(map);
+
+		const marker = new mapboxgl.Marker({
+			draggable: true
+		})
+			.setLngLat(coordinat1)
+			.addTo(map);
+
+		const coordinates = document.getElementById('coordinates');
+		function onDragEnd() {
+			coordinat1 = marker.getLngLat();
+		}
+
+		marker.on('dragend', onDragEnd);
+	}
+	let ofButton = 1;
+	let address1;
+	let address2;
+	let hidden = false;
+</script>
+
+<div class="grid grid-cols-1 gap-3 mb-6">
+	<div class="flex flex-col">
+		<span class="text-[#8e8e8e] mb-3 text-sm">Укажите маршрут</span>
+		<div class=" relative">
+			<span class="font-medium absolute  p-4 rounded-l-xl   h-full ">Откуда</span>
+			<button
+				class="font-medium absolute right-0  p-4 rounded-l-xl   h-full"
+				on:click={() => updateSet()}>Карта</button
+			>
+
+			<form class="">
+				<input
+					bind:value={address1}
+					autocomplete="city state street-address1"
+					placeholder="Омск, ул.Мира"
+					class=" border-[#e8e8e8]/75 font-sans focus:border-[#5BC43A] pl-24 focus:outline-none w-[100%] border p-4 rounded-2xl"
+					type="text"
+					name=""
+					id=""
+				/>
+			</form>
+		</div>
+	</div>
+	<div class="flex flex-col">
+		<div class=" relative">
+			<span class=" font-medium  absolute  p-4 rounded-l-xl   h-full  ">Куда</span>
+			<form>
+				<input
+					bind:value={address2}
+					autocomplete="street-address1"
+					placeholder="Омск, ул.Лукашевича"
+					class=" border-[#e8e8e8]/75 font-sans focus:border-[#5BC43A] pl-24 focus:outline-none w-[100%] border p-4 rounded-2xl"
+					type="text"
+					name=""
+					id=""
+				/>
+			</form>
+		</div>
+	</div>
 </div>
+{#if hidden}
+	<div id="map" class=" relative h-[30rem] top-0 z-0 ">
+		<div class="z-10 absolute w-full bottom-0">
+			{#if ofButton === 2}
+				<button
+					on:click={() => addCoordinat()}
+					class=" bg-[#5BC43A] z-10 p-3 rounded-2xl  w-full   py-4   font-semibold text-white"
+					>Первая точка</button
+				>
+			{:else if ofButton === 3}
+				<button
+					on:click={() => addCoordinat2()}
+					class=" bg-[#5BC43A]  z-10 p-3 rounded-2xl w-full  py-4   font-semibold text-white"
+					>Вторая точка</button
+				>
+			{/if}
+		</div>
+	</div>
+{/if}
--- src/routes/towtruck/+page.svelte
+++ src/routes/towtruck/+page.svelte
@@ -1,1 +1,119 @@
-<div>Hello</div>
+<script lang="ts">
+	import InputAddress from '$lib/components/InputAddress.svelte';
+	import carModel from './carModel.json';
+
+	function brands() {
+		return carModel.map((car) => car.brand);
+	}
+	let selected = 'Марка';
+	function models(cars) {
+		carModel.find(({ brand, models }) => {
+			if (brand === cars) {
+				model = models;
+			}
+			return;
+		});
+	}
+	let model = [];
+</script>
+
+<div class="px-5 mb-12">
+	<div>
+		<h1 class=" font-bold text-xl my-4 mb-7">Заказать эвакуатор</h1>
+	</div>
+
+	<div class="flex mb-6  flex-col">
+		<span class="text-[#8e8e8e] mb-3 text-sm">Контактные данные</span>
+
+		<input
+			placeholder="Номер телефона"
+			class="p-4 border-[#e8e8e8]/75 font-sans focus:border-[#5BC43A]  focus:outline-none border py-4 rounded-2xl"
+			type="text"
+			name=""
+			id=""
+		/>
+	</div>
+	<InputAddress />
+	<div class="flex flex-col">
+		<span class="text-[#8e8e8e] mb-3  text-sm">Марка и модель автомобиля</span>
+
+		<div class=" flex gap-3 ">
+			<select
+				bind:value={selected}
+				on:change={() => models(selected)}
+				class="form-select appearance-none
+            block
+            w-1/2
+            px-4
+            py-4
+            text-base
+            font-normal
+            bg-white bg-clip-padding bg-no-repeat
+             border border-solid border-[#e8e8e8]/75
+            transition
+            ease-in-out rounded-2xl
+            m-0
+            focus:text-gray-700 focus:bg-white focus:border-[#5BC43A] focus:outline-none"
+				aria-label="Default select example"
+			>
+				<option class=" font-semibold" selected>Марка</option>
+
+				{#each brands() as question}
+					<option value={question}>
+						{question}
+					</option>
+				{/each}
+			</select>
+			<select
+				class="form-select  appearance-none
+            block
+            w-1/2
+            px-4
+            py-4
+            text-base
+            font-normal
+            bg-white bg-clip-padding bg-no-repeat
+            border border-solid border-[#e8e8e8]/75
+            transition
+            ease-in-out rounded-2xl
+            m-0
+            focus:text-gray-700 focus:bg-white focus:border-[#5BC43A] focus:outline-none"
+				aria-label="Default select example"
+			>
+				<option class=" font-semibold" selected>Модель</option>
+
+				{#each model as m}
+					<option value={m}>
+						{m}
+					</option>
+				{/each}
+			</select>
+		</div>
+		<div class="mt-4">
+			<button class=" border-b">Сложность погрузки</button>
+		</div>
+	</div>
+	<div class="mt-6 border-t-2 border-dotted">
+		<div class="flex items-center justify-between">
+			<span class="my-4 text-[#8e8e8e] text">Стоимость:</span>
+			<p class=" text-xl font-semibold font-sans">{selected === 'BMW' ? '1990' : '2390'}₽</p>
+		</div>
+		<a href="/order/hello-world">
+			<button class=" bg-[#5BC43A] p-3 mt-2 rounded-2xl w-full   py-4   font-semibold text-white"
+				>Оставить заявку</button
+			>
+		</a>
+	</div>
+</div>
+
+<style>
+	@media all and (display-mode: standalone) {
+		.box {
+			position: fixed;
+			left: 20px;
+			bottom: 100px;
+			margin: 0;
+			width: 90%;
+		}
+	}
+</style>
